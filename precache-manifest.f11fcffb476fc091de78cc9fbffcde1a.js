3d0214027bd8e81d1e790a9619b70ed9f6879e70
sjyjytu <1332372004@qq.com>
2019-03-03 10:26:53 +0800

    Updates

---
+++ precache-manifest.f11fcffb476fc091de78cc9fbffcde1a.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b16618433ed6882841a2",
+    "url": "/ketianya/static/js/main.b1661843.chunk.js"
+  },
+  {
+    "revision": "b8d8ae7d7df83f9791ee",
+    "url": "/ketianya/static/js/runtime~main.b8d8ae7d.js"
+  },
+  {
+    "revision": "77054a235012b59fbd02",
+    "url": "/ketianya/static/js/2.77054a23.chunk.js"
+  },
+  {
+    "revision": "9d8dfb7dab5ce6a6a667c9a9f72b10dc",
+    "url": "/ketianya/index.html"
+  }
+];
